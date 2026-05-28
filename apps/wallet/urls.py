47913ab39a897ddaadb1de7d212883d3a3c1f156
CarlosBarriosSanchez <bsanchezcarlosd@uss.edu.pe>
2026-05-28 17:38:40 -0500

    Carlos

--- apps/wallet/urls.py
+++ apps/wallet/urls.py
@@ -1,8 +1,1 @@
-from django.urls import path
 
-from apps.wallet.views import BalanceView, DepositView
-
-urlpatterns = [
-    path("balance/", BalanceView.as_view(), name="wallet-balance"),
-    path("deposit/", DepositView.as_view(), name="wallet-deposit"),
-]
